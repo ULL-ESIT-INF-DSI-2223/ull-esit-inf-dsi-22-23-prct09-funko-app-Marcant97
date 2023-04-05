0677d739e431526bf44d20bc521e791a4ff12887
Marco Barroso <alu0101386560@ull.edu.es>
2023-04-05 10:25:20 +0000

    reorganización

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export enum Tipo {
+  Pop = "Pop!",
+  PopRides = "Pop! Rides",
+  VynilSoda = "Vynil Soda",
+  VynilGold = "Vynil Gold",
+  VynilPlatinum = "Vynil Platinum",
+}
+
+export enum Genero {
+  Animacion = "Animacion",
+  PeliculasYTV = "Peliculas y TV",
+  Videojuegos = "Videojuegos",
+  Deportes = "Deportes",
+  Musica = "Música",
+  Anime = "Anime",
+}
